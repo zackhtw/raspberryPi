a44183105f89fc28951dcfbb28ea9d21dc4eebc0
zackhtw <39162580+zackhtw@users.noreply.github.com>
2018-05-16 15:17:23 -0500

    Update MeArm.py

--- MeArm.py
+++ MeArm.py
@@ -10,62 +10,62 @@ AMaxs = [180,165,180,180] #Maximum angles for Servos 0-3
 ACurs = AInis #Current angles being set as the intial angles
 Step = 5
 for i in range(4):
-print(SNums[i],AInis[i],AMins[i],AMaxs[i],ACurs[i])
+  print(SNums[i],AInis[i],AMins[i],AMaxs[i],ACurs[i])
 
 os.system('sudo /home/pi/PiBits/ServoBlaster/user/servod –idle-timeout=2000') #This line is sent to command line to start the servo controller
 
 #inc listens for all key presses. On certain presses in the if statements below it either calls a process to add or subtract from the current servo angle.
 def inc(event):
-print “pressed”, repr(event.char)
-if repr(event.char) == "'a'":
-AAdd(0)
-if repr(event.char) == "'d'":
-ASub(0)
+  print ("pressed", repr(event.char))
+  if repr(event.char) == "'a'":
+    AAdd(0)
+  if repr(event.char) == "'d'":
+    ASub(0)
 
-if repr(event.char) == “‘w’”:
-AAdd(1)
-if repr(event.char) == “‘s’”:
-ASub(1)
+  if repr(event.char) == "'w'":
+    AAdd(1)
+  if repr(event.char) == "'s'":
+    ASub(1)
 
-if repr(event.char) == “‘j’”:
-AAdd(2)
-if repr(event.char) == “‘l’”:
-ASub(2)
+  if repr(event.char) == "'j'":
+    AAdd(2)
+  if repr(event.char) == "'l'":
+    ASub(2)
 
-if repr(event.char) == “‘i’”:
-AAdd(3)
-if repr(event.char) == “‘k’”:
-ASub(3)
+  if repr(event.char) == “‘i’”:
+    AAdd(3)
+  if repr(event.char) == "'k'":
+    ASub(3)
 
 def callback(event):
-frame.focus_set()
+  frame.focus_set()
 
 def AAdd(Servo):
-if ACurs[Servo] < AMaxs[Servo]:
-ACurs[Servo] = ACurs[Servo]+Step
-# micro = (1000 + (ACurs[Servo] * 5.555))
-micro = (1000 + (ACurs[Servo] * 8.3333))
-print(ACurs[Servo],micro)
-os.system(“echo %d=%dus > /dev/servoblaster” % (SNums[Servo],micro))
-else:
-print “Max Angle Reached”,SName[Servo],”Servo”
+  if ACurs[Servo] < AMaxs[Servo]:
+    ACurs[Servo] = ACurs[Servo]+Step
+    # micro = (1000 + (ACurs[Servo] * 5.555))
+    micro = (1000 + (ACurs[Servo] * 8.3333))
+    print(ACurs[Servo],micro)
+    os.system("echo %d=%dus > /dev/servoblaster" % (SNums[Servo],micro))
+  else:
+    print ("Max Angle Reached",SName[Servo],"Servo")
 
 def ASub(Servo):
-if ACurs[Servo] > AMins[Servo]:
-ACurs[Servo] = ACurs[Servo]-Step
-# micro = (1000 + (ACurs[Servo] * 5.555))
-micro = (1000 + (ACurs[Servo] * 8.3333))
-print(ACurs[Servo],micro)
-os.system(“echo %d=%dus > /dev/servoblaster” % (SNums[Servo],micro))
+  if ACurs[Servo] > AMins[Servo]:
+    ACurs[Servo] = ACurs[Servo]-Step
+    # micro = (1000 + (ACurs[Servo] * 5.555))
+    micro = (1000 + (ACurs[Servo] * 8.3333))
+    print(ACurs[Servo],micro)
+    os.system("echo %d=%dus > /dev/servoblaster" % (SNums[Servo],micro))
 
-else:
-print “Min Angle Reached”,SName[Servo],”Servo”
+  else:
+  print ("Min Angle Reached",SName[Servo],"Servo")
 
 frame = Frame(root, width=500, height=300)
-boxtext = Label(root, text=”Click this box for keyboard command of the MeArm. Use the a d s w j l i and k keys for control.”)
+boxtext = Label(root, text="Click this box for keyboard command of the MeArm. Use the a d s w j l i and k keys for control.")
 boxtext.pack()
-frame.bind(“<Key>“,inc)
-frame.bind(“<Button-1>”, callback)
+frame.bind("<Key>",inc)
+frame.bind("<Button-1>", callback)
 frame.pack()
 
 root.mainloop()
